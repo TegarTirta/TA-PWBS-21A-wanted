2a1ad8d2d11564b7dacc32a9a982b824bca01b53
TegarTirta <tegartirtalaksana@gmail.com>
2023-11-27 00:10:58 +0700

    master = upload frontend pertama

---
+++ frontend/app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+// Panggil Function ViewPage
+import ViewPage from './view/page'
+
+export default function MainPage() {
+  return (
+  <div>
+    ViewPage
+
+  </div>
+  )
+}
---
+++ frontend/app/view/page.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function ViewPage() {
+  return <div>Ini Halaman Data Mahasiswa</div>;
+}
